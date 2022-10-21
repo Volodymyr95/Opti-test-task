c02da4617a4b27654ccb2a473c851344ec6e24bf
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-10-22 01:32:33 +0300

    initial commit

---
+++ src/test/java/opti/test/OptiTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package opti.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
